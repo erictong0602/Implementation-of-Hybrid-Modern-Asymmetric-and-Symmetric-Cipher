c61b6f06d369f7b7836d94ad1b25a5e0ff825976
Eric Tong <erictong19990@gmail.com>
2023-04-02 14:44:13 +0800

    [2022-04-02] ET- Springboot quick start

---
+++ src/test/java/com/assignment/tsc711/Tsc711ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.tsc711;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tsc711ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
